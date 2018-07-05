98f751490e713127672630d8ea57abf03c129212
Samip <sneu012@gmail.com>
2018-07-05 16:17:43 -0400

    Added onTap handler to konva stage

--- src/ComplaintImageFactory.js
+++ src/ComplaintImageFactory.js
@@ -43,6 +43,18 @@ export default function(Complaints, SelectImages) {
       }, this.notify);
     };
 
+    handleTap = evt => {
+      let { currentTarget: { pointerPos }, target: { className } } = evt;
+      if (className !== "Image") {
+        return;
+      }
+
+      this.setState(state => {
+        let complaints = state.complaints.concat({ pos: pointerPos });
+        return Object.assign({}, state, { complaints });
+      }, this.notify);
+    };
+
     handleTextChange = (i, text) => {
       this.setState(({ url, complaints }) => {
         let complaint = Object.assign({}, complaints[i], { text });
@@ -118,7 +130,10 @@ export default function(Complaints, SelectImages) {
               selected={url}
               onSelect={this.changeImage}
             />
-            <Stage onClick={this.handleClick} ref="stage">
+            <Stage
+              onClick={this.handleClick}
+              onTap={this.handleTap}
+              ref="stage">
               <Layer visible ref="layer">
                 <Image ref="image" image={image} />
                 {complaints.map((conf, i) => {
